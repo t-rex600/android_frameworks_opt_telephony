0c6c00d49dd1f1ce74cd84c0f6f02ba5fd2a7344
Antonio Marín Cerezuela <amarin@swissms.ch>
2013-05-27 11:38:57 +0200

    Telephony API extension v2
    Added the following notifications:
    
    public void notifyPreciseCallState(Phone sender);
    public void notifyDisconnectCause(Connection.DisconnectCause cause, int preciseCause);
    public void notifyPreciseDataConnectionFailed(Phone sender, String reason, String apnType, String apn, String failCause);
    
    Change-Id: If3b88c679507c529b746046c4a17cf6d9974bd09

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -399,6 +399,8 @@ public class GSMPhone extends PhoneBase {
     /*package*/ void
     notifyDisconnect(Connection cn) {
         mDisconnectRegistrants.notifyResult(cn);
+
+        mNotifier.notifyDisconnectCause(cn.getDisconnectCause(), cn.getPreciseDisconnectCause());
     }
 
     void notifyUnknownConnection() {
